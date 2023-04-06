f7dd189c1736412fc7d87143e0ed1dc0146c7b3d
Aratskov <ifromukraine2022@gmail.com>
2023-04-06 11:33:03 +0300

    add

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,36 +1,36 @@
-import { useParams } from "react-router-dom";
-import { getMovieCast } from "../Service/ServiceApi";
-import { useEffect, useState } from "react";
-import { Gallery } from "./Cast.styled";
+import { useParams } from 'react-router-dom';
+import { getMovieCast } from '../Service/ServiceApi';
+import { useEffect, useState } from 'react';
+import { Gallery } from './Cast.styled';
 
 const Cast = () => {
   const { moviesId } = useParams();
-  const [casts, setCast] = useState([]);
+  const [casts, setCast] = useState(null);
 
   useEffect(() => {
     getMovieCast(moviesId)
       .then(setCast)
-      .catch((error) => console.log(error.message));
+      .catch(error => console.log(error.message));
   }, [moviesId]);
 
   if (!casts) return;
   const { cast } = casts;
-
+  
   return (
     <Gallery>
-      {cast.map(({ id, profile_path, name }) => {
-        if (!profile_path) return;
-        return (
-          <li key={id}>
-            <img
-              src={`https://image.tmdb.org/t/p/w500/${profile_path}`}
-              alt={name}
-              lazy="loading"
-            />
-            <p>{name}</p>
-          </li>
-        );
-      })}
+      {cast.map(
+        ({ id, profile_path, name }) =>
+          profile_path && (
+            <li key={id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w500/${profile_path}`}
+                alt={name}
+                lazy="loading"
+              />
+              <p>{name}</p>
+            </li>
+          )
+      )}
     </Gallery>
   );
 };

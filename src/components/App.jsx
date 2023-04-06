6f7bfac7de284071e10c1b494d18b0282210ba7b
Aratskov <ifromukraine2022@gmail.com>
2023-04-06 13:08:34 +0300

    add *

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,12 @@
-import { Suspense, lazy } from "react";
-import { Routes, Route } from "react-router-dom";
-import SharedLayout from "./SharedLayout/SharedLayout";
-import Home from "../pages/Home/Home";
+import { Suspense, lazy } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import SharedLayout from './SharedLayout/SharedLayout';
+import Home from '../pages/Home/Home';
 
-const Movies = lazy(() => import("../pages/Movies/Movies"));
-const MoviesDetail = lazy(() => import("../pages/MoviesDetail/MoviesDetail"));
-const Cast = lazy(() => import("./Cast/Cast"));
-const Reviews = lazy(() => import("./Reviews/Reviews"));
+const Movies = lazy(() => import('../pages/Movies/Movies'));
+const MoviesDetail = lazy(() => import('../pages/MoviesDetail/MoviesDetail'));
+const Cast = lazy(() => import('./Cast/Cast'));
+const Reviews = lazy(() => import('./Reviews/Reviews'));
 
 const App = () => {
   return (
@@ -15,9 +15,25 @@ const App = () => {
         <Route index element={<Home />} />
         <Route path="movies" element={<Movies />} />
         <Route path="movies/:moviesId" element={<MoviesDetail />}>
-          <Route path="cast" element={<Suspense fallback={null}><Cast /></Suspense>} />
-          <Route path="reviews" element={<Suspense fallback={null}><Reviews /></Suspense>} />
+          <Route
+            path="cast"
+            element={
+              <Suspense fallback={null}>
+                <Cast />
+              </Suspense>
+            }
+          />
+          <Route
+            path="reviews"
+            element={
+              <Suspense fallback={null}>
+                <Reviews />
+              </Suspense>
+            }
+          />
+          <Route path="*" element={<Navigate to="/" />} />
         </Route>
+        <Route path="*" element={<Navigate to="/" />} />
       </Route>
     </Routes>
   );
